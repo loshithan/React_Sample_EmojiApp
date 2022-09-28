2d237143102167e54cb607b55be4184830656534
loshithan paramaguru <loshithanparamaguru77@gmail.com>
2022-09-28 04:05:51 +0000

    Update 2 files

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+const numbers = [2,2,3,4,5,7]
+
+const newNumbs = numbers.map(function(x){
+    return x*x;
+})
+console.log(newNumbs);
